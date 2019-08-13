e14deaa99069903bd636587df3076ed4179137ba
Varundeepak <49053269+Varundeepak@users.noreply.github.com>
2019-08-13 18:42:14 +0530

    Create hours.cpp

---
+++ hours.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b=0;
+    cin>>a;
+    while(a>60)
+    {
+        b+=1;
+        a-=60;
+    }
+    if(a==60) 
+    {
+        b+=1;
+        a-=60;
+    }
+    cout<<b<<" "<<a;
+    return 0;
+}
