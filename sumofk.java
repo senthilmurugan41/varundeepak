7dc53f64e90ca8de452660d9a08985c81f4f53d3
Varundeepak <49053269+Varundeepak@users.noreply.github.com>
2019-08-13 18:32:02 +0530

    Create sumofk.java

---
+++ sumofk.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class sumofk
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int[] c=new int[a];
+        for(int i=0;i<a;i++) c[i]=s.nextInt();
+        int sum=0;
+        for(int i=0;i<b;i++) sum+=c[i];
+        System.out.println(sum);
+    }
+}
