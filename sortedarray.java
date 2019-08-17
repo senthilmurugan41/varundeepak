aa0904e054798dc1eecea049f0482a9eee63a606
Varundeepak <49053269+Varundeepak@users.noreply.github.com>
2019-08-17 23:49:49 +0530

    Create sortedarray.java

---
+++ sortedarray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++) a[i]=s.nextInt();
+        Arrays.sort(a);
+        for(int i=0;i<n;i++) System.out.print(a[i]+" ");
+    }
+}
+}
