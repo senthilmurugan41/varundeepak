a71fd2bf45f3412ce3bb09cd3818c3f1ff9918f9
Varundeepak <49053269+Varundeepak@users.noreply.github.com>
2019-08-17 21:20:27 +0530

    Create leapyear.c

---
+++ leapyear.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int year;
+    scanf("%d", &year);
+    if ((year % 400) == 0)
+        printf("yes");
+    else if ((year % 100) == 0)
+        printf("no");
+    else if ((year % 4) == 0)
+        printf("yes");
+    else
+        printf("no");
+    return 0;
+}
