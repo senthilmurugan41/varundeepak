0edf89b5a92dc413c6ad9d4157edbec83f9add59
Varundeepak <49053269+Varundeepak@users.noreply.github.com>
2019-08-17 23:56:43 +0530

    Create median.java

---
+++ median.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class median
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++) a[i]=s.nextInt();
+        Arrays.sort(a);
+        if(n%2!=0)
+        System.out.println(a[n/2]);
+        else
+        System.out.println((a[(n-1)/2]+a[n/2])/2);
+    }
+}
