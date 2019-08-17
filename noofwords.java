b45bd7e8fd5e16251d78772d1232d812db151355
Varundeepak <49053269+Varundeepak@users.noreply.github.com>
2019-08-18 00:29:25 +0530

    Create noofwords.java

---
+++ noofwords.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class noofwords {
+    public static void main(String args[] )
+    {
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        int c=a.length();
+        char[] b=new char[c];
+        for(int i=0;i<c;i++) b[i]=a.charAt(i);
+        int count=0;
+        for(int i=0;i<c;i++)
+        {
+            if(b[i]==' ') 
+            count++;
+            else continue;
+        }
+        System.out.println(count+1);
+    }
+}
+	
